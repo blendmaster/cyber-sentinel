5b9718590b10bdd1ca01393f929d31d2b1977f4e
jaredhance <jaredhance@gmail.com>
2012-07-16 19:36:49 +0000

    Lots of stuff

--- src/sate/cybersentinel/analysis/MessageGraph.java
+++ src/sate/cybersentinel/analysis/MessageGraph.java
@@ -1,15 +1,23 @@
 package sate.cybersentinel.analysis;
 
+<<<<<<< HEAD
 import sate.cybersentinel.analysis.Graph.InteractionGraphEdge;
 import sate.cybersentinel.analysis.Graph.InteractionGraph;
 import sate.cybersentinel.analysis.Graph.InteractionGraphVertex;
 import java.util.LinkedHashMap;
+=======
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+>>>>>>> Lots of stuff
 import java.util.List;
 import java.util.Map;
 
 import sate.cybersentinel.message.user.User;
 
 public class MessageGraph {
+<<<<<<< HEAD
     
     private InteractionGraph graph;
     private Map<User, List<ConversationCycle>> interactions;
@@ -123,3 +131,109 @@ public class MessageGraph {
     }
     
 }
+=======
+	private List<Message> messages;
+	private Map<String, List<Integer>> responsesMap = new HashMap<>();
+	private Map<String, List<Double>> scaledResponsesMap = new HashMap<>();
+	private List<Integer> autoResponse = new ArrayList<>();
+	private List<Double> scaledAutoResponse = new ArrayList<>();
+	
+	public MessageGraph(List<Message> messages) {
+		this.messages = messages;
+	}
+	
+	public void resolveConversationCycles(String uuid) {
+		int personCount = 0;
+		int i = 0;
+		while(i < messages.size()) {
+			Message message = messages.get(i);
+			if(message.getSenderUUID().equals(uuid)) {
+				if(i + 1 >= messages.size()) {
+					break;
+				}
+				
+				i++;
+				
+				List<String> personsTemp = new ArrayList<>();
+				int responseCount = 0;
+				
+				while(i < messages.size() && !messages.get(i).equals(uuid)) {
+					message = messages.get(i);
+					
+					String other = message.getSenderUUID();
+					personsTemp.add(other);
+					
+					List<Integer> responses;
+					if(responsesMap.containsKey(other)) {
+						responses = responsesMap.get(other);
+					}
+					else {
+						responses = new ArrayList<Integer>();
+						Integer[] zeroFiller = new Integer[personCount];
+						Arrays.fill(zeroFiller, 0);
+						responses.addAll(Arrays.asList(zeroFiller));
+					}
+					
+					responses.add(1);
+					responsesMap.put(other, responses);
+				
+					responseCount++;
+					i++;
+				}
+				i--;
+				
+				for(Iterator<String> iter = responsesMap.keySet().iterator(); iter.hasNext();) {
+					String person2 = iter.next();
+					if(!personsTemp.contains(person2)) {
+						List<Integer> responses = responsesMap.get(person2);
+						responses.add(0);
+						responsesMap.put(person2, responses);
+					}
+				}
+				
+				if(responseCount > 0) {
+					autoResponse.add(1);
+					personCount++;
+				}
+				
+				i++;
+			}
+			
+			autoResponse.remove(personCount - 1);
+			for(Iterator<String> iter = responsesMap.keySet().iterator(); iter.hasNext();) {
+				String personID = iter.next();
+				List<Integer> responses = responsesMap.get(personID);
+				responses.remove(personCount - 1);
+				responsesMap.put(personID, responses);
+			}
+			
+			personCount--;
+			
+			scaleResponses(personCount);
+		}
+		
+		
+	}
+
+	private void scaleResponses(int personCount) {
+		scaledResponsesMap = new HashMap<>();
+		for(Iterator<String> iter = scaledResponsesMap.keySet().iterator(); iter.hasNext();) {
+			List<Double> responses2 = new ArrayList<>();
+			String personID = iter.next();
+			List<Integer> responses = responsesMap.get(personID);
+			int sum = 0;
+			for(int response : responses) {
+				sum += response;
+				double scaledResponse = Math.tanh((double) sum / (personCount + 1));
+				responses2.add(scaledResponse);
+			}
+			scaledResponsesMap.put(personID, responses2);
+		}
+		
+		scaledAutoResponse = new ArrayList<>();
+		for(int j = 01; j <= personCount; j++) {
+			scaledAutoResponse.add(Math.tanh((double)(j + 1)/(personCount + 1)));
+		}
+	}
+}
+>>>>>>> Lots of stuff
