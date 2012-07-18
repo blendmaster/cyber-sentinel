77cb07ab34ad10d18f0020db886417466f3e6826
osesina <osesina@da447836-2e1a-ffe9-3c67-377700fac1a8>
2012-07-18 07:37:31 +0000

    graph statistics stuff Moved GraphConverter

---
+++ trunk/src/sate/cybersentinel/analysis/Graph/VertexStats.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sate.cybersentinel.analysis.Graph;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Isaac
+ */
+public interface VertexStats extends Serializable{
+
+    public static enum VertexGroup{MAIN_GRAPH, WEAK_CONNECTED, STRONG_CONNECTED, COMMUNITY_MODULARITY};
+//    public void setClusterCoefficient(double val);
+    public double getClusterCoefficient();
+
+//    public void setBetweenessCentrality(double val);
+    public double getBetweenessCentrality();
+
+//    public void setCutPoint(boolean bool);
+    public boolean isCutPoint();
+
+    public int inDegree();
+
+    public int inDegreeWeight();
+
+    public int outDegree();
+
+    public int outDegreeWeight();
+
+    public int Degree();
+
+    public int DegreeWeight();
+
+    public String toStringHeader();
+
+    public String toString();
+}
